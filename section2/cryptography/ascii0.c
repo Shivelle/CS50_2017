c85adb4c51e3afa0cfe2e3213da53cfdbe581fcc
Sarah Maria Leidisch <shivelle@gmx.net>
2017-08-05 06:38:06 +0000

    ascii cryptography

---
+++ section2/cryptography/ascii0.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+int main(void) {
+
+        for(int i = 65; i <= 90; i++) {
+            printf("%c is %i\n", (char) i,  i); 
+        }
+    
+    
+}
---
+++ section2/cryptography/ascii1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+int main(void) {
+
+        for(int c = 'A'; c <= 'Z'; c++) {
+            printf("%c is %i\n", c, (int) c); 
+        }
+    
+    
+}
